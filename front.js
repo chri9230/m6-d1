6ace07b36a55ad000024be7bf70048d6d627ae2b
chri9230 <cm@emmetistore.it>
2024-06-15 19:30:46 +0200

    la somma e la sottrazione del carrello funzionano correttamente

--- front.js
+++ front.js
@@ -1,3 +1,5 @@
+let totaleCarrello = 0 
+
 document.addEventListener("DOMContentLoaded", async () => {
     displayProducts()
 })
@@ -35,28 +37,32 @@ function displayProducts() {
 }
 
 function addCart(articolo, price) {
-    let contenitoreLi = document.getElementById('listaCarrello')
-    
-    // console.log(articolo)
-    
-    contenitoreLi.innerHTML += 
-    `
-    <li id='art'>
-     ${articolo} - ${price} € 
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16" onclick='elimina(event)'>
-  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-</svg>
-    </li>
+    let contenitoreLi = document.getElementById('listaCarrello');
 
-    `
+    totaleCarrello += parseFloat(price);
+    document.getElementById('totale').innerHTML = totaleCarrello.toFixed(2) + ' €';
+
+    let li = document.createElement('li');
+    li.id = 'art';
+    li.innerHTML = `${articolo} - ${price} € 
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16" onclick='elimina(event, ${price})'>
+            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+        </svg>`;
+    contenitoreLi.appendChild(li);
 }
 
 function svuotaCarrello() {
-    document.getElementById('listaCarrello').innerHTML = ''
+    document.getElementById('listaCarrello').innerHTML = '';
+    totaleCarrello = 0;
+    document.getElementById('totale').innerHTML = totaleCarrello.toFixed(2) + ' €';
 }
 
-function elimina(event) {
-
-    let liDaRimuovere = event.target.closest('li')
-    liDaRimuovere.remove()
+function elimina(event, price) {
+    let liDaRimuovere = event.target.closest('li');
+    liDaRimuovere.remove();
+    totaleCarrello -= parseFloat(price);
+    document.getElementById('totale').innerHTML = totaleCarrello.toFixed(2) + ' €';
 }
+
+
+
